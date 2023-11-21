2d870081fefbd98ce65f2b064da8591fd8ca708a
F1reFinger <180121847@aluno.unb.br>
2023-11-21 00:25:33 -0300

    code tested and working

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@ BluetoothSerial SerialBT;
 byte BTData;
 
 // GPIOs DC motor
-int DCmotorInput1 = 32;               // IN1
-int DCmotorInput2 = 34;               // IN2
+int DCmotorInput1 = 21;               // IN1
+int DCmotorInput2 = 22;               // IN2
 int DCmotorInput3 = 25;               // IN3
 int DCmotorInput4 = 27;               // IN4
 
@@ -27,65 +27,48 @@ void setup() {
 }
 
 void loop() {
-
-
-  if(SerialBT.available())
-  {
+  if (SerialBT.available()) {
     BTData = SerialBT.read();
-  }
 
-  if(BTData == 'S')
-  {
-    // Stops the DC motor
-    Serial.println("Motor stopped");
-    digitalWrite(DCmotorInput1, LOW);
-    digitalWrite(DCmotorInput2, LOW);
-    digitalWrite(DCmotorInput3, LOW);
-    digitalWrite(DCmotorInput4, LOW); 
-    delay(1000);
-  }
+    if (BTData == 'S') {
+      // Stops the DC motor
+      Serial.println("Motor stopped");
+      digitalWrite(DCmotorInput1, LOW);
+      digitalWrite(DCmotorInput2, LOW);
+      digitalWrite(DCmotorInput3, LOW);
+      digitalWrite(DCmotorInput4, LOW); 
+      delay(1000);
+    } else if (BTData == 'F') {
+      // Moves the DC motor forward with speed according to PWM duty cycle
+      Serial.println("Moving Forward");
+      digitalWrite(DCmotorInput3, LOW);
+      digitalWrite(DCmotorInput4, HIGH); 
+      digitalWrite(DCmotorInput1, LOW);
+      digitalWrite(DCmotorInput2, HIGH); 
+      delay(1000);
+    } else if (BTData == 'B') {
+        // Moves the DC motor backwards with speed according to PWM duty cycle
+        Serial.println("Moving Backwards");
+        digitalWrite(DCmotorInput3, HIGH);
+        digitalWrite(DCmotorInput4, LOW);
+        digitalWrite(DCmotorInput1, HIGH);
+        digitalWrite(DCmotorInput2, LOW);
+        delay(1000);
+    } else if (BTData == 'I') {
+        digitalWrite(DCmotorInput1, LOW);
+        digitalWrite(DCmotorInput2, HIGH); 
+        digitalWrite(DCmotorInput3, LOW);
+        digitalWrite(DCmotorInput4, LOW); 
+        delay(1000);
+    } else if (BTData == 'D') {
+      digitalWrite(DCmotorInput1, LOW);
+      digitalWrite(DCmotorInput2, LOW);
+      digitalWrite(DCmotorInput3, LOW);
+      digitalWrite(DCmotorInput4, HIGH);
+      delay(1000);
+    }
 
-
-  if(BTData == 'F')
-  {
-    // Moves the DC motor forward with speed according to PWM duty cycle
-    Serial.println("Moving Forward");
-    digitalWrite(DCmotorInput3, LOW);
-    digitalWrite(DCmotorInput4, HIGH); 
-    digitalWrite(DCmotorInput1, LOW);
-    digitalWrite(DCmotorInput2, HIGH); 
-    
-    delay(1000);
-  }
-
-  if(BTData == 'B')
-  {
-    // Moves the DC motor backwards with speed according to PWM duty cycle
-    Serial.println("Moving Backwards");
-    digitalWrite(DCmotorInput3, HIGH);
-    digitalWrite(DCmotorInput4, LOW);
-    digitalWrite(DCmotorInput1, HIGH);
-    digitalWrite(DCmotorInput2, LOW);
-    
-    delay(1000);
-
-  }
-
-  if(BTData == 'I')
-  {
-    digitalWrite(DCmotorInput1, LOW);
-    digitalWrite(DCmotorInput2, HIGH); 
-    digitalWrite(DCmotorInput3, LOW);
-    digitalWrite(DCmotorInput4, LOW); 
-    delay(1000);
-  }
-
-  if(BTData == 'D')
-  {
-    digitalWrite(DCmotorInput1, LOW);
-    digitalWrite(DCmotorInput2, LOW);
-    digitalWrite(DCmotorInput3, LOW);
-    digitalWrite(DCmotorInput4, HIGH);
-    delay(1000);
+    // Reset BTData after executing the action
+    BTData = 'E'; // Set it to an empty character or another value
   }
 }
